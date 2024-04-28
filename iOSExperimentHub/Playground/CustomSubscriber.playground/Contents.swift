a2bc4c376f7f88ba1533ef4c38b40ab867b0d230
Huy Pham <huypham8501@gmail.com>
2024-04-29 02:00:06 +0700

    custom combine subscriber

--- iOSExperimentHub/Playground/CustomSubscriber.playground/Contents.swift
+++ iOSExperimentHub/Playground/CustomSubscriber.playground/Contents.swift
@@ -1,22 +1,32 @@
 import Combine
 
-class CustomSubscriber: Subscriber {
+// Define CustomSubscriber as a normal class
+class CustomSubscriber {
+    var value: Int = 0
+}
+
+// Extend CustomSubscriber to conform to the Subscriber protocol
+extension CustomSubscriber: Subscriber {
     typealias Input = Int
     typealias Failure = Never
 
     func receive(subscription: Subscription) {
-        subscription.request(.max(3))
+        subscription.request(.max(1))
     }
 
     func receive(_ input: Int) -> Subscribers.Demand {
+        self.value = input
         print("Value:", input)
-        return .max(1)
+        return .none
     }
 
     func receive(completion: Subscribers.Completion<Never>) {
         print("Completion: \(completion)")
     }
 }
-let publisher = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].publisher
+
+// Use CustomSubscriber
+let publisher = [1, 2, 3, 4, 5].publisher
 let subscriber = CustomSubscriber()
 publisher.subscribe(subscriber)
+
